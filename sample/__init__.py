4a2803cd4e6e2ecdd46206a75fd3eca6a46c5ee5
sejoung kim <sejoung@gmail.com>
2023-12-22 13:28:58 +0900

    lint 수정

--- sample/__init__.py
+++ sample/__init__.py
@@ -1,1 +1,9 @@
+"""Module providing a function printing Sample version."""
 from .core import hmm
+
+SAMPLE_VERSION = "0.1"
+
+
+def print_python_version():
+    """Print the Sample"""
+    print(SAMPLE_VERSION)
